763ee9237bc723847ebfab031537c19e7544620c
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-09-07 15:03:50 -0500

    feat(model): implement constructor with defensive copy for products

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/Sale.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/Sale.java
@@ -13,6 +13,13 @@ public class Sale {
     private Seller seller;
     private List<Product> products;
   
+    public Sale(String id, LocalDate date, Customer customer, Seller seller, List<Product> products) {
+        this.id = id;
+        this.date = date;
+        this.customer = customer;
+        this.seller = seller;
+        this.products = products != null ? new ArrayList<>(products) : new ArrayList<>();
+    }
     
     
 }
