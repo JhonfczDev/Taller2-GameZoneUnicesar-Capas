01d2009f3c3ee1b67fd424e38dff2569d3ab426b
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-09-07 15:31:11 -0500

    feat(persistence): create SaleRepository class structure with file path constants

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/SaleRepository.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/SaleRepository.java
@@ -1,5 +1,25 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence;
 
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Product;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Sale;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SaleRepository {
+    
+    private static final String FILE_PATH = "data/sales.txt";
+    private static final String DELIMITER = ";";
+    private static final String PRODUCT_SEPARATOR = ",";
+    
+    public SaleRepository() {
+        ensureFileExists();
+    }
+    
     
 }
