90134dcc0a3f1e958d86b613dedb5d9e1a5fb140
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-09-07 14:58:05 -0500

    feat(model): create Sale class structure and attributes

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/Sale.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/Sale.java
@@ -1,8 +1,17 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.model;
 
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Product;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sale {
     
-  
+    private String id;
+    private LocalDate date;
+    private Customer customer;
+    private Seller seller;
+    private List<Product> products;
   
     
     
