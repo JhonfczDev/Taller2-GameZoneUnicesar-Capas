bc5f898104beb288ba9feeb1fd89faeb0c57a049
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-09-08 14:44:16 -0500

    feat(ui): add ConsoleUI base structure and main menu navigation

---
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/ui/UI.java
@@ -0,0 +1,17 @@
+package co.unicesar.edu.taller2.gamezoneunicesar.capas.ui;
+
+import service.PersonService;
+import service.ProductService;
+import service.SaleService;
+import model.Customer;
+import model.Product;
+import model.Sale;
+import model.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UI {
+    
+}
