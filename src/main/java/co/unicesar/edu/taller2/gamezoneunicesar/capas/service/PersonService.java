8358a1896ae9b28315fe670ca0954a1762cf7225
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-08 19:58:27 -0500

    feat: create of the getAllCustomer and getAllSeller methods

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/PersonService.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/PersonService.java
@@ -1,8 +1,9 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.service;
-import java.util.List;
+import java.util.*;
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence.PersonRepository;
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Person;
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Customer;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Seller;
 
 /**
  * Service layer that acts as an intermediary between the business logic
@@ -30,5 +31,41 @@ public class PersonService {
 
         persons.add(c);
         repository.saveAll(persons);
+    }
+
+    /**
+     * Retrieves all registered customers by filtering the full list
+     * of people, keeping only those that are instances of Customer.
+     * @return list of all customers
+     */
+    public List<Customer> getAllCustomers() {
+        List<Person> persons = repository.loadAll();
+        List<Customer> customers = new ArrayList<>();
+
+        for (Person person : persons) {
+            if (person instanceof Customer) {
+                customers.add((Customer) person);
+            }
+        }
+
+        return customers;
+    }
+
+    /**
+     * Retrieves all registered sellers by filtering the full list
+     * of people, keeping only those that are instances of Seller.
+     * @return list of all sellers
+     */
+    public List<Seller> getAllSellers() {
+        List<Person> persons = repository.loadAll();
+        List<Seller> sellers = new ArrayList<>();
+
+        for (Person person : persons) {
+            if (person instanceof Seller) {
+                sellers.add((Seller) person);
+            }
+        }
+
+        return sellers;
     }
 }
