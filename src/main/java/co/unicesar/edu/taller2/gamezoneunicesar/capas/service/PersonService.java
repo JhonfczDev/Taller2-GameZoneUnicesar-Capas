e90049193f7fd00621c58a434446ca516fca8431
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-08 19:39:11 -0500

    feat: create the registerCustomer method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/PersonService.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/PersonService.java
@@ -1,5 +1,8 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.service;
+import java.util.List;
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence.PersonRepository;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Person;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Customer;
 
 /**
  * Service layer that acts as an intermediary between the business logic
@@ -15,5 +18,17 @@ public class PersonService {
      */
     public PersonService(PersonRepository repository) {
         this.repository = repository;
+    }
+
+    /**
+     * Registers a new customer: loads the current list of people,
+     * adds the new customer, and saves the updated full list.
+     * @param c the customer to register
+     */
+    public void registerCustomer(Customer c) {
+        List<Person> persons = repository.loadAll();
+
+        persons.add(c);
+        repository.saveAll(persons);
     }
 }
