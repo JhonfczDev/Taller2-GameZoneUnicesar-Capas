e17dff5ffbb998b4c013591a8562c04d78ee6649
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-14 14:57:34 -0500

    feat: ceation of the saveAll method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionRepository.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionRepository.java
@@ -1,6 +1,11 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence;
 
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.PercentageDiscount;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.CategoryDiscount ;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.BulkPurchaseDiscount;
+
 import java.io.*;
+import java.util.List;
 
 public class PromotionRepository {
     private File file;
@@ -10,6 +15,59 @@ public class PromotionRepository {
     public PromotionRepository(String filePath) {
         this.file = new File(filePath);
         ensureFileExists();
+    }
+
+    public void saveAll(List<Promotion> promotions){
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (Promotion promotion : promotions) {
+
+                if (promotion instanceof PercentageDiscount) {
+                    PercentageDiscount percentage = (PercentageDiscount) promotion;
+                    String line = "PERCENTAGE,"
+                            + percentage.getId() + ","
+                            + percentage.getName() + ","
+                            + percentage.getStartDate() + ","
+                            + percentage.getEndDate() + ","
+                            + percentage.getPercentage();
+
+                    writer.write(line);
+                    writer.newLine();
+
+                } else if (promotion instanceof CategoryDiscount) {
+                    CategoryDiscount category = (CategoryDiscount) promotion;
+
+                    String line = "CATEGORY,"
+                            + category.getId() + ","
+                            + category.getName() + ","
+                            + category.getStartDate() + ","
+                            + category.getEndDate() + ","
+                            + category.getPercentage() + ","
+                            + category.getTargetCategory();
+
+                    writer.write(line);
+                    writer.newLine();
+
+                } else if (promotion instanceof BulkPurchaseDiscount) {
+                    BulkPurchaseDiscount bulk = (BulkPurchaseDiscount) promotion;
+
+                    String line = "CATEGORY,"
+                            + bulk.getId() + ","
+                            + bulk.getName() + ","
+                            + bulk.getStartDate() + ","
+                            + bulk.getEndDate() + ","
+                            + bulk.getMinQuantity() + ","
+                            + bulk.getPercentage();
+
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+
+        } catch (IOException e) {
+
+            throw new RuntimeException("Error saving promotions...", e);
+        }
     }
 
     private void ensureFileExists() {
