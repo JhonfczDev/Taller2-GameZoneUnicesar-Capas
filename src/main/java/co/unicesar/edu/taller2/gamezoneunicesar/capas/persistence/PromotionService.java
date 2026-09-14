0823528cbff278838f4b071aba2a7a354090c15a
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-14 16:37:47 -0500

    feat: creation of the registerPercentageDiscount method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionService.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionService.java
@@ -7,4 +7,15 @@ public class PromotionService {
     public PromotionService(PromotionRepository repository) {
         this.repository = repository;
     }
+
+    public void registerPercentageDiscount(String id, String name, LocalDate startDate,
+                                           LocalDate endDate, double percentage) {
+        
+        PercentageDiscount promotion = new PercentageDiscount(id, name, startDate, endDate, percentage);
+        List<Promotion> promotions = repository.loadAll();
+
+        promotions.add(promotion);
+
+        repository.saveAll(promotions);
+    }
 }
