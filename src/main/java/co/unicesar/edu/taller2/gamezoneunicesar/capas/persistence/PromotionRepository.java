c79d0cbf7ac7ce57f71f98fbc1b020e3b1157439
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-14 14:38:19 -0500

    feat: creation and implementation of the ensureFileExists method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionRepository.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/PromotionRepository.java
@@ -1,6 +1,6 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence;
 
-import java.io.File;
+import java.io.*;
 
 public class PromotionRepository {
     private File file;
@@ -9,5 +9,20 @@ public class PromotionRepository {
 
     public PromotionRepository(String filePath) {
         this.file = new File(filePath);
+        ensureFileExists();
+    }
+
+    private void ensureFileExists() {
+        try {
+            File parent = file.getParentFile();
+            if (parent != null && !parent.exists()) {
+                parent.mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating the promotions file: " + e.getMessage());
+        }
     }
 }
