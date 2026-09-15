0c75df882e80ae3bfc1f9c456b4d28f9e701f00a
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-14 19:28:16 -0500

    feat: creation and implementation of the ensureFileExists method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/ReturnRepository.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/ReturnRepository.java
@@ -1,6 +1,7 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ReturnRepository {
 
@@ -10,5 +11,20 @@ public class ReturnRepository {
 
     public ReturnRepository(String filepath) {
         this.file = new File(filepath);
+        ensureFileExists();
+    }
+
+    private void ensureFileExists() {
+        try {
+            File parent = file.getParentFile();
+            if (parent != null && !parent.exists()) {
+                parent.mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating the returns file: " + e.getMessage());
+        }
     }
 }
