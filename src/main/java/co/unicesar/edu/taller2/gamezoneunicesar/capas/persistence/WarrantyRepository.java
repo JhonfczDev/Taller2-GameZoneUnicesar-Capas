a62e4a0ff99dc9f09e309a5b7d6eecc47a5a1b87
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-16 17:48:23 -0500

    feat: creation and implementation of the ensureFileExists method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/WarrantyRepository.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/persistence/WarrantyRepository.java
@@ -19,5 +19,20 @@ public class WarrantyRepository {
         this.file = new File(fileName);
         this.saleService = saleService;
         this.productService = productService;
+        ensureFileExists();
+    }
+
+    private void ensureFileExists() {
+        try {
+            File parent = file.getParentFile();
+            if (parent != null && !parent.exists()) {
+                parent.mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating the warranties file: " + e.getMessage());
+        }
     }
 }
