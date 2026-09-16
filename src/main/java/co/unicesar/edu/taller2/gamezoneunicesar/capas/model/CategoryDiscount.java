7fa30d1f78b936af560a7dba0b6da8b1d4c91a12
elkinjimenez7 <edanieljimenez@unicesar.edu.co>
2026-09-15 21:50:12 -0500

    feat: create class CategoryDiscount extends Discount

---
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/CategoryDiscount.java
@@ -0,0 +1,44 @@
+package co.unicesar.edu.taller2.gamezoneunicesar.capas.model;
+
+import java.time.LocalDate;
+
+public class CategoryDiscount extends Discount {
+    private double percentage;
+    private String targetCategory;
+
+    public CategoryDiscount(String id, String name, LocalDate startDate, LocalDate endDate, double percentage, String targetCategory) {
+        super(id, name, startDate, endDate);
+        this.percentage = percentage;
+        this.targetCategory = targetCategory;
+    }
+
+    @Override
+    public double calculateDiscount(Sale sale) {
+        double categoryTotal = 0.0;
+        
+        // null validation for products list
+        for (Product product : sale.getProducts()) {
+            if (product != null && product.targetCategory().equalsIgnoreCase(product.getCategory())) {
+                categoryTotal += product.getPrice();
+            }
+        }
+        return categoryTotal * (this.percentage / 100);
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getTargetCategory() {
+        return targetCategory;
+    }
+
+    public void setTargetCategory(String targetCategory) {
+        this.targetCategory = targetCategory;
+    }
+
+}
