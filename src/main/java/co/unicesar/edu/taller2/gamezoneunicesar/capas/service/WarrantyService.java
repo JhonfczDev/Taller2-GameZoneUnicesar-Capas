78614313a052bb12e3042359364d440f5c58a3fa
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-16 21:07:55 -0500

    feat: creation of the assignBasicWarranty method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/WarrantyService.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/WarrantyService.java
@@ -1,8 +1,14 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.service;
 
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.BasicWarranty;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Product;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Sale;
+import co.unicesar.edu.taller2.gamezoneunicesar.capas.model.Warranty;
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence.WarrantyRepository;
 
-import java.util.UUID;
+import java.time.LocalDate;
+import java.util.*;
+import java.io.*;
 
 public class WarrantyService {
 
@@ -14,5 +20,19 @@ public class WarrantyService {
 
     private String generateWarrantyId() {
         return "W" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+    public BasicWarranty assignBasicWarranty(Product product, Sale sale, LocalDate startDate) {
+        String fabricDefects = "Covers manufacturing defects";
+        int duration = 12;
+        String priceProduct = String.valueOf(product.getPrice());
+
+        BasicWarranty warranty = new BasicWarranty(fabricDefects, duration, priceProduct, generateWarrantyId());
+
+        List<Warranty> warranties = repository.loadAll();
+        warranties.add(warranty);
+        repository.saveAll(warranties);
+
+        return warranty;
     }
 }
