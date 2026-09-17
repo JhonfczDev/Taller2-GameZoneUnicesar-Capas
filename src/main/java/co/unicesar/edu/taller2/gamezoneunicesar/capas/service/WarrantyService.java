de3583cd4cf80205cac5bd0422576a8a4c15d17e
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-09-16 20:54:39 -0500

    feat: creation of the generateWarrantyId method

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/WarrantyService.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/service/WarrantyService.java
@@ -2,11 +2,17 @@ package co.unicesar.edu.taller2.gamezoneunicesar.capas.service;
 
 import co.unicesar.edu.taller2.gamezoneunicesar.capas.persistence.WarrantyRepository;
 
+import java.util.UUID;
+
 public class WarrantyService {
 
     private WarrantyRepository repository;
 
     public WarrantyService(WarrantyRepository repository) {
         this.repository = repository;
+    }
+
+    private String generateWarrantyId() {
+        return "W" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
     }
 }
