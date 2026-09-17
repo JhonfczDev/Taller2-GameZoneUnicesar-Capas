350908966e355cf210356d1cd46888c12f00c9d2
elkinjimenez7 <edanieljimenez@unicesar.edu.co>
2026-09-16 21:06:39 -0500

    feat: the class BasicWarranty extends Warranty was modified

--- src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/BasicWarranty.java
+++ src/main/java/co/unicesar/edu/taller2/gamezoneunicesar/capas/model/BasicWarranty.java
@@ -1,9 +1,26 @@
 package co.unicesar.edu.taller2.gamezoneunicesar.capas.model;
 
+import java.time.LocalDate;
+
 public class BasicWarranty extends Warranty {
 
-    public BasicWarranty(String fabricDefects, int duration, String priceProduct, String id) {
-        super(fabricDefects, duration, priceProduct, id);
+    public BasicWarranty(String id, Product product, Sale sale, LocalDate startDate) {
+        super(id, product, sale, startDate);
+    }
+    
+    @Override
+    public int getDurationInMonths(){
+        return 6;
+    }
+
+    @Override
+    public String getWarrantyType(){
+        return "Garantia Basica";
+    }
+
+    @Override
+    public double getAdditionalCost(){
+        return 0.0; // it doesn't have additional cost
     }
     
 }
